b45b1d4442653d2ae85e7bd81b68c6bc8344d2b8
jeje <jerome.botiveau@epitech.eu>
2022-07-21 13:14:12 +0200

    shema change

--- rpg/db/schema.rb
+++ rpg/db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_20_090816) do
 ActiveRecord::Schema[7.0].define(version: 2022_07_20_093335) do
   create_table "answers", force: :cascade do |t|
     t.text "description"
