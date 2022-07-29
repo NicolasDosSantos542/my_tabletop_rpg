24307253714b1562874c33cd8a63a732ec240183
Nico <nicolas.de-meyere-dos-santos@epitech.eu>
2022-07-29 12:12:39 +0200

    display choose_loot in fight page

--- rpg/app/services/loot_admin.rb
+++ rpg/app/services/loot_admin.rb
@@ -1,13 +1,18 @@
 # app/services/tweet_creator.rb
 class LootAdmin
-    def initialize(loot)
-      @loot = loot
+    def initialize()
     end
   
     def say_hello   
         return "hello toto #{@loot}"
      
     end
+    def select_loot(loot_id)
+      first = Loot.find(loot_id)
+      others = Loot.where("strength <= ?", [first.strength])
+      return [first,others[0],others[1]]
+
+    end
 end
   
-LootAdmin.new("beau gosse").say_hello
+# LootAdmin.new("beau gosse").say_hello
